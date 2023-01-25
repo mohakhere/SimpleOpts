080a50d7c41744b8149d8059a6eef8e7f735c9cd
Mohak <mohakthisside@gmail.com>
2023-01-25 23:45:37 +0530

    Added Flask for prime_opts

--- views.py
+++ views.py
@@ -1,13 +1,3 @@
-# find prime numbers in a range
-# Take a value from user, until which the prime numbers will be given
-# n = int(input("Enter the number: "))
-# for u in range(1, n+1):
-#     if u > 1:
-#         for i in range(2, u):
-#             if (u % i) == 0:
-#                 break
-#         else:
-#             print(u)
 from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='template')
