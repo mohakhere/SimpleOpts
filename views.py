4b540e51772e337ccc277ee8ddce308fe027fe0a
Mohak Sethi <107026265+mohakhere@users.noreply.github.com>
2023-02-25 21:48:15 +0530

    Update views.py

--- views.py
+++ views.py
@@ -17,6 +17,14 @@ def primes():
         return render_template('primes.html', primes=primelist)
     else:
         return render_template('primes.html')
+    
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run()
